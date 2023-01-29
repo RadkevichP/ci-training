eff4421b871dfd481dbbc53bc341a794fb69ef84
Pavel Radkevich <pradkevich@eisgroup.com>
2023-01-29 12:14:57 +0200

    Initial commit

---
+++ src/main/java/by/radkevich/citraining/CiTrainingApplication.java
@@ -0,0 +1,20 @@
+package by.radkevich.citraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+
+@SpringBootApplication(exclude = {
+        DataSourceAutoConfiguration.class,
+        DataSourceTransactionManagerAutoConfiguration.class,
+        HibernateJpaAutoConfiguration.class})
+
+public class CiTrainingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CiTrainingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/by/radkevich/citraining/CiTrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package by.radkevich.citraining;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiTrainingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
